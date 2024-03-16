518f29f3fe4d0a630baf1941f41df93507c8ef17
Juan Almenara <juanalmenara@gmail.com>
2024-03-15 23:30:10 -0500

    Update server.js with envio de texto

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 //Esto es solo una prueba, a ver si muere la "base de datos"
 var fs = require('fs');
 
-var data = fs.readFileSync('palabras.json');
-var palabras = JSON.parse(data);
+var DB = fs.readFileSync('DB.json');
+var textosGuardados = JSON.parse(DB);
 console.log('**Se leyó el archivo JSON**');
 
 var express = require('express');
@@ -14,56 +14,32 @@ var port = process.env.PORT;
 var server = app.listen(port, escuchando);
 
 function escuchando(){
-    console.log('**Escuchando el puerto 3000**');
+    console.log('**Escuchando el puerto 2727**');
 }
-
 app.use(express.static('website'));
+app.use(express.json({limit:'1mb'}));
 
-app.get('/agregar/:palabra/:puntaje', mandarPalabra);
-function mandarPalabra(pedido,respuesta){
-    var palabra = pedido.params.palabra;
-    var puntaje = Number(pedido.params.puntaje);
-    var respuesta2;
-    if(!puntaje){
-        respuesta2 = {
-            mensaje: "El puntaje es necesario!"
-        }
+app.get('/servicio', mandarTexto);
+function mandarTexto(pedido,respuesta){
+    console.log("** Han mandado algo **");
+    const data = pedido.body;
+    data.tiempoDeLlegada = Date.now();
+    
+    textosGuardados.push(texto);
+    data = JSON.stringify(textosGuardados);
+    fs.writeFile('DB.json', data, terminado);
+    function terminado(){
+        console.log('Se grabó en el archivo JSON');
     }
-    else{
-        palabras[palabra] = puntaje;
-        data = JSON.stringify(palabras);
-        fs.writeFile('palabras.json', data, terminado);
-        respuesta2 = {
-            mensaje: "Gracias por su palabra."
-        }
-        function terminado(){
-            console.log('Se grabó en el archivo JSON');
-        } 
-    }
-    respuesta.send(respuesta2);
+    respuesta.json({
+        status: '**Se pudo mandar!',
+        data: data
+    });
 }
 
-app.get('/todos',mandarTodos);
-function mandarTodos(pedido,respuesta){
-    respuesta.send(palabras);
-}
-
-app.get('/buscar/:palabra', busqueda);
-function busqueda(pedido,respuesta){
-    var palabra = pedido.params.palabra;
-    var respuesta2;
-    if(palabras[palabra]){
-        respuesta2 = {
-            status: 'Encontrado!',
-            palabra: palabra,
-            puntaje: palabras[palabra]
-        }
-    }
-    else{
-        respuesta2 = {
-            status: 'No encontrado',
-            palabra: palabra
-        }
-    }
-    respuesta.send(respuesta2);
+app.get('/servicio', recibirTexto);
+function recibir(pedido,respuesta){
+    respuesta.json({
+       data: DB
+    }); 
 }
